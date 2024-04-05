c42ba08b5f01e0c46da76dafe11e4991b1729286
Graham Ross <graham.ross@crick.ac.uk>
2024-04-05 17:19:46 +0100

    refactored TCP settings

--- src/napari_sbem_viewer/_widgets/sbemimage_integration/tcp_settings.py
+++ src/napari_sbem_viewer/_widgets/sbemimage_integration/tcp_settings.py
@@ -1,5 +1,5 @@
 import napari
-from qtpy.QtWidgets import QLabel, QLineEdit, QPushButton, QSpinBox, QGroupBox, QGridLayout, QHBoxLayout
+from qtpy.QtWidgets import QLabel, QLineEdit, QPushButton, QSpinBox, QGroupBox, QGridLayout, QHBoxLayout, QVBoxLayout
 from qtpy.QtCore import Qt
 from threading import Thread
 
@@ -12,26 +12,25 @@ class TCPSettings(QGroupBox):
         super().__init__("TCP settings")
         self.viewer = viewer
         self.tcp_server = tcp_server
-        self.setLayout(QGridLayout())
+        self.setLayout(QVBoxLayout())
         
+        tcp_form_layout = QGridLayout()
         self.host_line_edit = QLineEdit(text=self.tcp_server.host)
-        self.layout().addWidget(QLabel("Host", alignment=Qt.AlignLeft), 0, 0)
-        self.layout().addWidget(self.host_line_edit, 0, 1)
-
+        tcp_form_layout.addWidget(QLabel("Host", alignment=Qt.AlignLeft), 0, 0)
+        tcp_form_layout.addWidget(self.host_line_edit, 0, 1)
         self.port_spinbox = QSpinBox(maximum=99999, value=self.tcp_server.port)
-        self.layout().addWidget(QLabel("Port"), 1, 0)
-        self.layout().addWidget(self.port_spinbox, 1, 1)
+        tcp_form_layout.addWidget(QLabel("Port"), 1, 0)
+        tcp_form_layout.addWidget(self.port_spinbox, 1, 1)
+        self.layout().addLayout(tcp_form_layout)
         
+        tcp_control_layout = QHBoxLayout()
         self.start_server_button = QPushButton("Start server", enabled=True)
         self.start_server_button.clicked.connect(self._on_click_start_server)
-
+        tcp_control_layout.addWidget(self.start_server_button)
         self.stop_server_button = QPushButton("Stop server", enabled=False)
         self.stop_server_button.clicked.connect(self._on_click_stop_server)
-        
-        layout = QHBoxLayout()
-        layout.addWidget(self.start_server_button)
-        layout.addWidget(self.stop_server_button)
-        self.layout().addLayout(layout, 2, 0, 1, 2)
+        tcp_control_layout.addWidget(self.stop_server_button)
+        self.layout().addLayout(tcp_control_layout)
 
     def _on_click_start_server(self):
         self.tcp_server.host = self.host_line_edit.text()
