e70e82f6fccb1f1de981bff2ba544049c7173c2b
Graham Ross <graham.ross@crick.ac.uk>
2024-04-12 18:10:26 +0100

    Fixing ROIs - work in progress

--- src/napari_sbem_viewer/_widgets/select_rois/roi_list.py
+++ src/napari_sbem_viewer/_widgets/select_rois/roi_list.py
@@ -17,34 +17,72 @@ class ROIList(QGroupBox):
         # self.shapes_layer = shapes_layer
         
         self.roi_list_widget = QListWidget()
+        self.roi_list_widget.itemClicked.connect(self._on_click_roi_list)
         self.layout().addWidget(self.roi_list_widget, 0, 0, 1, 2)
+        
         
         self.add_button = QPushButton("Add")
         self.layout().addWidget(self.add_button, 1, 0)
         self.remove_button = QPushButton("Remove", enabled=False)
         self.layout().addWidget(self.remove_button, 1, 1)
         
+    def focus_viewer_on_roi(self, roi_idx):
+        center_coords = self.parent.bbox_layer.data_to_world(get_roi_center(self.parent.bbox_layer.data[roi_idx]))
+        self.viewer.camera.center = center_coords
+        self.viewer.dims.set_point(0, center_coords[0])
+        
     def _reset_z_viewer(self, z: int):
         # reset the z viewer to the z slice of the ROI in world coords
         self.viewer.dims.set_point(0, z)
+        
+    def _on_click_roi_list(self, item):
+        current_row = self.roi_list_widget.row(item)
+        
+        if current_row == -1:
+            self.remove_button.setEnabled(False)
+            return
+
+        if current_row == self.roi_list_widget.count() - 1:
+            self.remove_button.setEnabled(True)
+        else:
+            self.remove_button.setEnabled(False)
+        
+        # highlight the current point in the viewer
+        bbox_layer = self.parent.bbox_layer
+        # if current_row < len(bbox_layer.data):
+        #     bbox_layer.selected_data = [current_row]
+        #     bbox_layer._set_highlight(force=True)
+        
+        self.focus_viewer_on_roi(current_row)
+        bbox_layer.selected_data = [current_row]
+        print(bbox_layer.selected_data)
+        bbox_layer._set_highlight(force=True)
             
     def _on_update_bbox(self, event):
         """
         Called when the shapes layer is updated by either adding or removing
         ROIs directly in the viewer.
         """
-        if not hasattr(event, 'action'):
-            return
+        # event with no action attribute is called when the shape has finished drawing (???) 
+        # - use this to focus on the ROI after it has been added.
         bbox_layer = self.parent.bbox_layer
-        # if event.action == ActionType.ADDING or event.action == ActionType.REMOVING:
-        #     self.current_z_slice = self.viewer.dims.point[0]
+        if not hasattr(event, 'action'):
+            if hasattr(event, 'data_indices'):
+                self.roi_list_widget.addItem(f'ROI {len(bbox_layer.data)}')
+                self.roi_list_widget.setCurrentRow(self.roi_list_widget.count() - 1)
+                self._on_click_roi_list(self.roi_list_widget.item(self.roi_list_widget.count() - 1))
+            return
         if event.action == ActionType.ADDED:
-            if len(bbox_layer.data) < self.roi_list_widget.count():
+            if len(bbox_layer.data) <= self.roi_list_widget.count():
                 return
-            # # TODO: change z coord of rectangle to nearest integer so it aligns with overview stack
-            self.roi_list_widget.addItem(f'ROI {len(bbox_layer.data)}')
         if event.action == ActionType.REMOVED:
-            self.roi_list_widget.takeItem(event.data_indices[0])
-        # if event.action == ActionType.REMOVED or event.action == ActionType.ADDED:
-        #     self.viewer.dims.set_point(0, self.current_z_slice)        
-        #     print(f'set point to {self.current_z_slice}')
+            for idx in event.data_indices:
+                self.roi_list_widget.takeItem(idx)
+        
+        
+def get_roi_center(coords_list):
+    # calculate the min / max values of the x, y and z coordinates
+    min_coords = np.min(coords_list, axis=0)
+    max_coords = np.max(coords_list, axis=0)
+    center_coords = (min_coords + max_coords) / 2
+    return center_coords
--- src/napari_sbem_viewer/_widgets/select_rois/select_rois.py
+++ src/napari_sbem_viewer/_widgets/select_rois/select_rois.py
@@ -23,17 +23,15 @@ class SelectROIs(QWidget):
         self.roi_list = ROIList(self.viewer, parent=self)
         self.roi_list.add_button.clicked.connect(self._on_click_add)
         self.roi_list.remove_button.clicked.connect(self._on_click_remove)
-        # self.roi_list.roi_list_widget.itemClicked.connect(self._on_select_roi_list)
-        self.roi_list.roi_list_widget.currentItemChanged.connect(self._on_select_roi_list)
         
-        # self.roi_list.roi_list_widget.currentRowChanged.connect(self._on_change_roi_list)
+        self.roi_list.roi_list_widget.currentRowChanged.connect(self._on_change_roi_list)
         self.layout().addWidget(self.roi_list)
         
-        # self.adjust_roi = AdjustROI(self.viewer)
-        # self.adjust_roi.setVisible(False)
-        # self.layout().addWidget(self.adjust_roi)
-        # self.adjust_roi.starting_slice.valueChanged.connect(self._on_adjust_roi_starting_z)
-        # self.adjust_roi.ending_slice.valueChanged.connect(self._on_adjust_roi_ending_z)
+        self.adjust_roi = AdjustROI(self.viewer)
+        self.adjust_roi.setVisible(False)
+        self.layout().addWidget(self.adjust_roi)
+        self.adjust_roi.starting_slice.valueChanged.connect(self._on_adjust_roi_starting_z)
+        self.adjust_roi.ending_slice.valueChanged.connect(self._on_adjust_roi_ending_z)
         
         # self.stack_viewer = StackViewer(napari.Viewer(show=False))
         # self.layout().addWidget(self.stack_viewer)
@@ -42,26 +40,15 @@ class SelectROIs(QWidget):
         
         self.layout().addStretch(1)
         
-    # def _on_change_roi_list(self, item):
-    #     if item == -1:
-    #         self.adjust_roi.setVisible(False)
-    #         return
-    #     roi_coords = self.bbox_layer.data[item]
-    #     starting_slice = int(self.bbox_layer.data_to_world(roi_coords[0])[0])
-    #     ending_slice = int(self.bbox_layer.data_to_world(roi_coords[1])[0])
-    #     self.adjust_roi.starting_slice.setValue(starting_slice)
-    #     self.adjust_roi.ending_slice.setValue(ending_slice)
-    #     self.adjust_roi.setVisible(True)
-        
     def _on_adjust_roi_starting_z(self, value):
         z_data = self.bbox_layer.world_to_data((value, 0, 0))[0]
         self.bbox_layer.data[self.roi_list.roi_list_widget.currentRow()][::2, 0] = z_data
-        self.bbox_layer.refresh()
+        self.bbox_layer.data = self.bbox_layer.data
     
     def _on_adjust_roi_ending_z(self, value):
         z_data = self.bbox_layer.world_to_data((value, 0, 0))[0]
         self.bbox_layer.data[self.roi_list.roi_list_widget.currentRow()][1::2, 0] = z_data
-        self.bbox_layer.refresh()
+        self.bbox_layer.data = self.bbox_layer.data
           
     def _on_click_add(self):
         if self.bbox_layer is None:
@@ -88,13 +75,7 @@ class SelectROIs(QWidget):
         self.bbox_layer.selected_data = [selected_row]
         
         # remove the selected_row from napari
-        # roi_list = self.bbox_layer.data
-        # roi_list = np.delete(roi_list, selected_row, axis=0)
-        # self.bbox_layer.data = roi_list
         self.bbox_layer.remove_selected()
-        
-        # remove the selected_row from the points list
-        # self.roi_list.roi_list_widget.takeItem(selected_row)
         
         # remove the highlight from the viewer
         self.bbox_layer.selected_data = []
@@ -110,35 +91,19 @@ class SelectROIs(QWidget):
             self.bbox_layer = None
             self.roi_list.roi_list_widget.clear()
             # self.stack_viewer.remove_substack()
-        
-    def _on_select_roi_list(self, item):
-        current_row = self.roi_list.roi_list_widget.row(item)
-        
-        if current_row == -1:
-            self.roi_list.remove_button.setEnabled(False)
-            return
-
-        if current_row == self.roi_list.roi_list_widget.count() - 1:
-            self.roi_list.remove_button.setEnabled(True)
-        else:
-            self.roi_list.remove_button.setEnabled(False)
-        
-        # highlight the current point in the viewer
-        if current_row < len(self.bbox_layer.data):
-            self.bbox_layer.selected_data = [current_row]
-            self.bbox_layer._set_highlight(force=True)
             
-        center_coords = self.bbox_layer.data_to_world(get_roi_center(self.bbox_layer.data[current_row]))
-        self.viewer.camera.center = center_coords
-        self.viewer.dims.set_point(0, center_coords[0])
+    def _on_change_roi_list(self, current_row):
+        self._render_adjust_roi_widget(current_row)
         
-        self.bbox_layer.selected_data = [current_row]
-        self.bbox_layer._set_highlight(force=True)
+    
         
-        
-def get_roi_center(coords_list):
-    # calculate the min / max values of the x, y and z coordinates
-    min_coords = np.min(coords_list, axis=0)
-    max_coords = np.max(coords_list, axis=0)
-    center_coords = (min_coords + max_coords) / 2
-    return center_coords
+    def _render_adjust_roi_widget(self, idx):
+        if idx == -1:
+            self.adjust_roi.setVisible(False)
+            return
+        roi_coords = self.bbox_layer.data[idx]
+        starting_slice = int(self.bbox_layer.data_to_world(roi_coords[0])[0])
+        ending_slice = int(self.bbox_layer.data_to_world(roi_coords[1])[0])
+        self.adjust_roi.starting_slice.setValue(starting_slice)
+        self.adjust_roi.ending_slice.setValue(ending_slice)
+        self.adjust_roi.setVisible(True)
