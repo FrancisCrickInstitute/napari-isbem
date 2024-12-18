c2c6bebf30726c08e2878d660a5123c347ae22e3
Graham Ross <graham.ross@crick.ac.uk>
2024-12-18 14:21:08 +0000

    Improved roi combo box events

--- src/napari_sbem_viewer/_controllers/acquisition_controller.py
+++ src/napari_sbem_viewer/_controllers/acquisition_controller.py
@@ -30,16 +30,35 @@ class AcquisitionController:
         self.roi_settings.roi_combo_box.currentIndexChanged.connect(self._on_roi_layer_changed)
 
         # Init viewer events
-        self.acquisition_model.viewer.layers.events.removed.connect(self._update_roi_selections)
-        self.acquisition_model.viewer.layers.events.inserted.connect(self._update_roi_selections)
+        self.acquisition_model.viewer.layers.events.inserted.connect(self._on_add_layer)
+        self.acquisition_model.viewer.layers.events.removed.connect(self._on_remove_layer)
         self.acquisition_model.viewer.dims.events.current_step.connect(self._on_change_z_depth)
-        self._update_roi_selections()
-        
+        self._populate_roi_combo_box()
+
     def _on_change_z_depth(self):
         if not self.acquisition_model.live_viewer.image_dir:
             return
         viewer_z_depth = self.acquisition_model.get_viewer_z_depth()
         self.acquisition_info.viewer_z_depth.setText(f"{viewer_z_depth:.2f}µm")
+        
+    def _on_add_layer(self, event):
+        if not isinstance(event.value, Labels):
+            return
+        self.roi_settings.roi_combo_box.addItem(event.value.name)
+        
+    def _on_remove_layer(self, event):
+        if isinstance(event.value, Labels):
+            idx = self.roi_settings.roi_combo_box.findText(event.value.name)
+            if idx >= 0:
+                self.roi_settings.roi_combo_box.removeItem(idx)
+        if event.value == self.acquisition_model.live_viewer.layer:
+            self._on_reset_overview()
+            
+    def _populate_roi_combo_box(self):
+        self.roi_settings.roi_combo_box.addItem("")
+        for layer in self.acquisition_model.viewer.layers:
+            if isinstance(layer, Labels):
+                self.roi_settings.roi_combo_box.addItem(layer.name)
         
     def _on_click_start_server(self):
         self.acquisition_model.tcp_server.host = self.tcp_settings.host_line_edit.text()
@@ -83,20 +102,6 @@ class AcquisitionController:
     def _on_roi_layer_changed(self):
         roi_layer = self._get_roi_layer()
         self.acquisition_model.set_roi_layer(roi_layer)
-                
-    def _update_roi_selections(self):
-        layer_names = self._get_roi_layer_names()
-        roi_layer = self.roi_settings.roi_combo_box.currentText()
-        self.roi_settings.roi_combo_box.clear()
-        self.roi_settings.roi_combo_box.addItem("")
-        self.roi_settings.roi_combo_box.addItems(layer_names)
-    
-        # if the selected layer has been deleted, unselect from the combo boxes
-        if self.roi_settings.roi_combo_box.currentText() not in layer_names:
-            self.roi_settings.roi_combo_box.setCurrentIndex(0)
-            self.acquisition_model.roi_data.clear()
-        else:
-            self.roi_settings.roi_combo_box.setCurrentText(roi_layer)
         
     def _handle_overview_error(self, error):
         self.acquisition_settings.show_error("Error adding images", str(error))
