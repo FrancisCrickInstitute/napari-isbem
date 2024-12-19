6049ecc4a82b892f1810a68977987b6489b2c271
Graham Ross <graham.ross@crick.ac.uk>
2024-12-19 14:47:10 +0000

    Improve select images widget

--- src/napari_sbem_viewer/_views/registration/select_images.py
+++ src/napari_sbem_viewer/_views/registration/select_images.py
@@ -1,6 +1,6 @@
 import napari
 from napari.layers import Image, Labels
-from qtpy.QtWidgets import QGridLayout, QGroupBox, QComboBox, QLabel
+from qtpy.QtWidgets import QVBoxLayout, QHBoxLayout, QGroupBox, QComboBox, QLabel
 
 
 class SelectImages(QGroupBox):
@@ -10,51 +10,51 @@ class SelectImages(QGroupBox):
                  ):
         super().__init__("Select images", parent=parent)
         self.viewer = viewer
-        self.setLayout(QGridLayout())
+        self.setLayout(QVBoxLayout())
         
-        self.layout().addWidget(QLabel("Fixed image"))
         self.fixed_combo_box = QComboBox()
-        self.layout().addWidget(self.fixed_combo_box)
-        
-        self.layout().addWidget(QLabel("Moving image"))
         self.moving_combo_box = QComboBox()
-        self.layout().addWidget(self.moving_combo_box)
         
-        self.viewer.layers.events.removed.connect(self._update_selections)
-        self.viewer.layers.events.inserted.connect(self._update_selections)
+        fixed_lyt = QHBoxLayout()
+        fixed_lyt.addWidget(QLabel("Fixed"))
+        fixed_lyt.addWidget(self.fixed_combo_box, 1)
+        self.layout().addLayout(fixed_lyt)
         
-        self._update_selections()
+        moving_lyt = QHBoxLayout()
+        moving_lyt.addWidget(QLabel("Moving"))
+        moving_lyt.addWidget(self.moving_combo_box, 1)
+        self.layout().addLayout(moving_lyt)
+        
+        self.viewer.layers.events.removed.connect(self._on_remove_layer)
+        self.viewer.layers.events.inserted.connect(self._on_add_layer)
+        self._populate_combo_boxes()
         
     def get_moving_layer(self):
         return self._get_layer(self.moving_combo_box.currentText())
     
     def get_fixed_layer(self):
         return self._get_layer(self.fixed_combo_box.currentText())
-        
-    def _update_selections(self):
-        layer_names = self._get_image_layer_names()
-        
-        moving_layer = self.moving_combo_box.currentText()
-        self.moving_combo_box.clear()
-        self.moving_combo_box.addItems(layer_names)
-        
-        fixed_layer = self.fixed_combo_box.currentText()
-        self.fixed_combo_box.clear()
-        self.fixed_combo_box.addItems(layer_names)
     
-        # if the selected layer has been deleted, unselect from the combo boxes
-        if self.moving_combo_box.currentText() not in layer_names:
-            self.moving_combo_box.setCurrentText(None)
-        else:
-            self.moving_combo_box.setCurrentText(moving_layer)
-            
-        if self.fixed_combo_box.currentText() not in layer_names:
-            self.fixed_combo_box.setCurrentText(None)
-        else:
-            self.fixed_combo_box.setCurrentText(fixed_layer)
+    def _populate_combo_boxes(self):
+        for layer in self.viewer.layers:
+            if isinstance(layer, Image):
+                self.fixed_combo_box.addItem(layer.name)
+                self.moving_combo_box.addItem(layer.name)
+                
+    def _on_add_layer(self, event):
+        if not isinstance(event.value, Image):
+            return
+        self.fixed_combo_box.addItem(event.value.name)
+        self.moving_combo_box.addItem(event.value.name)
         
-    def _get_image_layer_names(self):
-        return [x.name for x in self.viewer.layers if (isinstance(x, Image) or isinstance(x, Labels))]
+    def _on_remove_layer(self, event):
+        if isinstance(event.value, Image):
+            idx = self.fixed_combo_box.findText(event.value.name)
+            if idx >= 0:
+                self.fixed_combo_box.removeItem(idx)
+            idx = self.moving_combo_box.findText(event.value.name)
+            if idx >= 0:
+                self.moving_combo_box.removeItem(idx)
         
     def _get_layer(self, layer_name):
         for layer in self.viewer.layers:
