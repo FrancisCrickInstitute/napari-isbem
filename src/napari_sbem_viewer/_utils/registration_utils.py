564bf28f9ddae18d7431e4719689e765b7052189
Graham Ross <graham.ross@crick.ac.uk>
2025-02-20 12:42:04 +0000

    Remove unnecessary registration functions

--- src/napari_sbem_viewer/_utils/registration_utils.py
+++ src/napari_sbem_viewer/_utils/registration_utils.py
@@ -8,98 +8,10 @@ from napari.layers import Image, Layer
 from scipy.spatial.transform import Rotation as R
 
 
-def quaternion_from_vectors(v1, v2):
-    k_cos_theta = np.dot(v1, v2)
-    k = np.linalg.norm(v1) * np.linalg.norm(v2)
-    return (*np.cross(v1, v2), k + k_cos_theta)
-
-
-def axis_angle_from_vectors(v1, v2):
-    # Calculate the rotation axis
-    axis = np.cross(v1, v2)
-    if np.linalg.norm(axis) == 0:
-        return np.array(v1), 0
-    
-    axis /= np.linalg.norm(axis)
-
-    # Calculate the angle of rotation
-    angle = np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
-
-    return axis, angle
-
-
-def line_parametric_equation(p1, p2, t):
-    # Given two points p1 and p2, calculate the point p on the line between p1 and p2 at parameter t
-    t = np.clip(t, 0, 1)
-    return p1 + t * (p2 - p1)
-
-
-def calculate_t(p1, p2, p):
-    # Given two points p1 and p2, and a point p, calculate the parameter t such that p = p1 + t(p2 - p1)
-    # e.g. how far along the line p1 to p2 is p
-    v = p2 - p1
-    w = p - p1
-    return np.dot(w, v) / np.dot(v, v)
-    
-    
-def find_intersections(b0, b1, p, v):
-    intersection_points = []
-    for point in [b0, b1]:
-        for direction in [[1, 0, 0], [0, 1, 0], [0, 0, 1]]:
-            dot_product = np.dot(v, direction)
-            distance_to_plane = np.dot(point - p, direction)
-            t = distance_to_plane / dot_product
-            intersection_point = p + t * v
-            # if the intersection point is within the bounds of the plane, add it to the list
-            if np.all(b0 <= intersection_point) and np.all(intersection_point <= b1):
-                intersection_points.append(intersection_point)
-    return intersection_points
-
-
-def rotation_matrix_from_vectors(vec1, vec2):
-    """Compute the rotation matrix to rotate vec1 onto vec2."""
-    # Normalize vectors
-    vec1 = vec1 / np.linalg.norm(vec1)
-    vec2 = vec2 / np.linalg.norm(vec2)
-
-    axis, angle = axis_angle_from_vectors(vec1, vec2)
-
-    return rotation_matrix_from_axis_angle(axis, angle)
-
-
 def rotation_matrix_from_zy_zx_angles(zy_angle, zx_angle):
     mat = np.eye(4)
     mat[:3, :3] = R.from_euler('zyx', [zy_angle, zx_angle, 0], degrees=True).as_matrix()
     return mat
-
-
-def rotation_matrix_from_axis_angle(axis, angle):
-    """
-    Convert an axis and an angle into a 4x4 transformation matrix.
-    
-    Parameters:
-    axis (array-like): A 3-element array-like structure representing the rotation axis.
-    angle (float): The rotation angle in radians.
-    
-    Returns:
-    np.ndarray: A 4x4 transformation matrix.
-    """
-    axis = np.array(axis)
-    axis = axis / np.linalg.norm(axis)  # Normalize the axis
-    x, y, z = axis
-    c = np.cos(angle)
-    s = np.sin(angle)
-    t = 1 - c
-    
-    # Create the rotation matrix
-    rotation_matrix = np.array([
-        [t*x*x + c,   t*x*y - s*z, t*x*z + s*y, 0],
-        [t*x*y + s*z, t*y*y + c,   t*y*z - s*x, 0],
-        [t*x*z - s*y, t*y*z + s*x, t*z*z + c,   0],
-        [0,           0,           0,           1]
-    ])
-    
-    return rotation_matrix
 
 
 def is_rotation_matrix(matrix):
@@ -152,60 +64,6 @@ def is_2d_affine_matrix(matrix):
         return False
 
     return True
-        
-        
-def get_transformation_matrix_2d(moving_points, fixed_points):
-    # convert coordinates from (y, x) to (x, y)
-    fixed_points = fixed_points[:, ::-1]
-    moving_points = moving_points[:, ::-1]
-    Rt, _ = cv2.estimateAffine2D(moving_points, fixed_points)
-    Rt = np.vstack([[Rt[0, 0], Rt[0, 1], Rt[1, 2]], 
-                    [Rt[1, 0], Rt[1, 1], Rt[0, 2]], 
-                    [0, 0, 1]])
-    return Rt
-
-
-def get_transformation_matrix_slices(reverse, z_offset, pts_fixed, pts_moving, scale=None):
-    T = np.eye(4)
-    
-    # scale transformation to offset the x and y scaling
-    if scale is not None:
-        T[1, 1] = scale[0]
-        T[2, 2] = scale[1]
-    
-    if reverse:
-        T[0, 0] *= -1  # flip z-axis
-    
-    if z_offset != 0:
-        T[0, 3] -= z_offset  # shift image to align with fixed image
-    
-    if pts_fixed is not None and pts_moving is not None:
-        T_2d = get_transformation_matrix_2d(pts_moving, pts_fixed)
-        rotation = np.arctan2(T_2d[1, 0], T_2d[0, 0])
-        scale_x = T_2d[0, 0] / np.cos(rotation)
-        scale_y = T_2d[1, 1] / np.cos(rotation)
-        translation_x = T_2d[1, 2]
-        translation_y = T_2d[0, 2]
-        
-        rotate_T = np.eye(4)
-        rotate_T[1, 1] = np.cos(rotation)
-        rotate_T[1, 2] = np.sin(rotation)
-        rotate_T[2, 1] = -np.sin(rotation)
-        rotate_T[2, 2] = np.cos(rotation)
-        
-        scale_T = np.eye(4)
-        scale_T[1, 1] = scale_y
-        scale_T[2, 2] = scale_x
-        
-        translate_T = np.eye(4)
-        translate_T[2, 3] = translation_x
-        translate_T[1, 3] = translation_y
-
-        T = np.matmul(scale_T, T)
-        T = np.matmul(rotate_T, T)
-        T = np.matmul(translate_T, T)
-    
-    return T
 
 
 def rotate_image_3d_sitk(image, quaternion, interpolator='linear'):
@@ -283,46 +141,6 @@ def transform_image_3d_sitk(image, transformation_matrix, interpolator='linear')
                                       interpolator,
                                       output_origin)
     return sitk.GetArrayFromImage(image_transformed).astype(image.dtype), min_coords
-
-
-def transform_image_3d(image, transformation_matrix):
-    transformation_matrix = np.linalg.inv(transformation_matrix)
-    return affine_transform(image, transformation_matrix, output_shape=image.shape)
-
-
-def rotate_image_3d(image, angle, axis):
-    # Create rotation object
-    rotation = Rotation.from_rotvec(angle * axis)
-
-    # Calculate center of rotation
-    center = np.array(image.shape) / 2
-
-    # Calculate translation to ensure the rotated image fits within the original image
-    max_translation = np.ceil(np.sqrt(np.sum(center ** 2)))
-    translation = max_translation - center
-
-    # Define transformation matrix (rotation + translation)
-    transformation_matrix = np.identity(4)
-    transformation_matrix[:3, :3] = rotation.as_matrix()
-    # transformation_matrix[:3, 3] = translation
-    corners = np.array([[0, 0, 0],
-                        [0, image.shape[0], 0],
-                        [image.shape[1], 0, 0],
-                        [0, 0, image.shape[2]],
-                        [image.shape[1], image.shape[0], 0],
-                        [image.shape[1], 0, image.shape[2]],
-                        [0, image.shape[0], image.shape[2]],
-                        [image.shape[1], image.shape[0], image.shape[2]]])
-    transformed_corners = np.dot(transformation_matrix[:3, :3], corners.T).T + center
-    min_corner = np.min(transformed_corners, axis=0)
-    max_corner = np.max(transformed_corners, axis=0)
-    transformation_matrix[:3, 3] = -min_corner
-    output_shape = np.ceil(max_corner - min_corner).astype(int)
-    
-    # Apply the transformation to the image
-    rotated_image = affine_transform(image, transformation_matrix, output_shape=output_shape).astype(image.dtype)
-    rotated_image = rotated_image / 65535
-    return rotated_image
 
 
 def offset_transform_matrix_z(mat, offset):
@@ -422,20 +240,6 @@ def calculate_normal(zy_degrees, zx_degrees):
     rotation_matrix = rotation_matrix_from_zy_zx_angles(zy_degrees, zx_degrees)
     normal = rotation_matrix[:3, :3] @ normal
     return normal.T[0]
-
-
-def rotate_layer(layer, v1, v2):
-    layer_type = 'image' if isinstance(layer, Image) else 'labels'
-    interpolator = 'linear' if layer_type == 'image' else 'nearest'
-    quaternion = quaternion_from_vectors(v1, v2)
-    if isinstance(layer.data, np.ndarray):
-        rotated_data = rotate_image_3d_sitk(layer.data, quaternion, interpolator)
-    else:
-        rotated_data = []
-        for pyramid_level in layer.data:
-            rotated_data.append(rotate_image_3d_sitk(pyramid_level.compute(), quaternion, interpolator))
-    rotated_layer = Layer.create(rotated_data, {'scale': layer.scale, 'name': layer.name + ' (rotated)'}, layer_type)
-    return rotated_layer
 
 
 def transform_layer(layer, affine_transform):
